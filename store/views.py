c4481ddf1869b033e7ba4a42065b707808cfd6cd
manoel.junior <manoel.junior@tceal.tc.br>
2022-12-19 21:24:59 -0300

    Criando mais alguns templates html

--- store/views.py
+++ store/views.py
@@ -14,10 +14,8 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
-        product_count - products.count()
+        product_count = products.count()
 
     context = {'products': products, 'product_count': product_count}
 
     return render(request, 'store/store.html', context)
-
-
